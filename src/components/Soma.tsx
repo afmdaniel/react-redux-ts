06c946fcc2062a1896baa177a603eed4725b2a51
afmdanielwin <danielfarias380@gmail.com>
2020-07-02 11:46:20 -0300

    Hooks no componente soma

--- src/components/Soma.tsx
+++ src/components/Soma.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { RootState } from '../store/storeConfig'
+import { selectNumeros } from '../store/storeConfig'
 
 import Card from './Card'
-
-const selectNumeros = (state: RootState) => state.numeros
 
 const Soma = () => {
     const { max, min } = useSelector(selectNumeros)
